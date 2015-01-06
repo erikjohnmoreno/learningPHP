c22cbc6ec9d4f222e81510be3284276cd2716229
erikjohnmoreno <e.moreno@klab.com>
2015-01-06 08:38:10 +0800

    commit objects folder with file creatingObjects.php and accessing_propertiesandmethods.php

---
+++ objects/accessing_propertiesandmethods.php
@@ -0,0 +1,9 @@
+<?php
+//Accessing Properties and Methods
+echo "Rasmus is {$rasmus->age} years old. \n"; 	//property access
+$rasmus->birthday();							//method call
+$rasmus->setAge(21);							//method call with arguments
+
+
+
+?>
