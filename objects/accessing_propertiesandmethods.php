b1c37153edb73a48b6fbde84739e53fc9e84fe13
erikjohnmoreno <e.moreno@klab.com>
2015-01-06 09:44:51 +0800

    modified accessing_propertiesandmethods.php

--- objects/accessing_propertiesandmethods.php
+++ objects/accessing_propertiesandmethods.php
@@ -1,9 +1,37 @@
 <?php
-//Accessing Properties and Methods
-echo "Rasmus is {$rasmus->age} years old. \n"; 	//property access
-$rasmus->birthday();							//method call
-$rasmus->setAge(21);							//method call with arguments
+//Accessing methods and properties
 
+
+class Person
+{
+	
+	public $name = '';
+
+	function __construct($newname, $age){
+
+			$this->name = $newname;
+
+	}
+
+
+	public function getName()
+	{
+		return $this->name;
+
+	}
+
+	public function setName($name)
+	{
+		$this->name = $name;
+	}
+
+}
+
+$f = new Person ("Fred", 35);
+$b = clone $f;
+$b->setName("Barney");
+
+printf("%s and %s are best friends.\n", $b->getName(), $f->getName());
 
 
 ?>
